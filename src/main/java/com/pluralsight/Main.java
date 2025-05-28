ba5a73dcd12d6fd7925275dd45afec04a701fed0
Josephvv7 <joseph.medina33@gmail.com>
2025-05-28 01:37:23 -0400

    added display UI to main class

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
 public class Main {
+    public static void main(String[] args) {
+        OrderMenu ui = new OrderMenu() {
+            @Override
+            public void display() {
+            }
+        };
+    }
 }
