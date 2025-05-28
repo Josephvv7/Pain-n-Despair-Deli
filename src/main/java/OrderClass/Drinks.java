db67febc9acc83133c098d5ec6fe3d8a48290c4b
Josephvv7 <joseph.medina33@gmail.com>
2025-05-28 01:34:02 -0400

    Added format to Drinks class getReceipt method

--- src/main/java/OrderClass/Drinks.java
+++ src/main/java/OrderClass/Drinks.java
@@ -1,11 +1,36 @@
 package OrderClass;
 
 public class Drinks {
-    public String getReceiptLine(){
-        return "Drink:";
+    private String size;
+    private String flavor;
+
+    public Drinks(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getFlavor() {
+        return flavor;
     }
 
     public double getPrice() {
-        return 0.00;
+        switch (size.toLowerCase()) {
+            case "small":
+                return 1.50;
+            case "medium":
+                return 2.00;
+            case "large":
+                return 2.50;
+            default:
+                return 0.00;
+        }
+    }
+
+    public String getReceiptLine() {
+        return String.format("Drink: %s %s - $%.2f", size, flavor, getPrice());
     }
 }
