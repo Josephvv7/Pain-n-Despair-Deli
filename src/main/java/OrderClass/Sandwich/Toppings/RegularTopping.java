307fa4b879842531cb8383b42df60ba9aea0ec4d
Josephvv7 <joseph.medina33@gmail.com>
2025-05-28 16:15:24 -0400

    Added list of regular toppings to regular toppings class

--- src/main/java/OrderClass/Sandwich/Toppings/RegularTopping.java
+++ src/main/java/OrderClass/Sandwich/Toppings/RegularTopping.java
@@ -14,4 +14,9 @@ public class RegularTopping extends ToppingsA {
     public String getType() {
         return "Regular";
     }
+
+    public static String[] getAvailableToppings() {
+        return new String[] { "Lettuce", "Tomato", "Onion", "Peppers", "Pickles", "Cucumbers", "Spinach" };
+    }
+
 }
